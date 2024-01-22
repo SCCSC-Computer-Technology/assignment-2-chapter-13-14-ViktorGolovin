da4d5e630e73251365c1919aab5dd2ba32b8dc21
ViktorGolovin <155990944+ViktorGolovin@users.noreply.github.com>
2024-01-21 20:52:45 -0500

    Chapter 14 Problem 6, Lab1

---
+++ CPT-206-Chapter-14-Problem-6-Viktor-Golovin/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace CPT_206_Chapter_14_Problem_6_Viktor_Golovin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.productDataGrid = new System.Windows.Forms.DataGridView();
+            this.ascendingToolStrip = new System.Windows.Forms.ToolStrip();
+            this.ascendingToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.productNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitsOnHandDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productDBDataSet = new CPT_206_Chapter_14_Problem_6_Viktor_Golovin.ProductDBDataSet();
+            this.productTableAdapter = new CPT_206_Chapter_14_Problem_6_Viktor_Golovin.ProductDBDataSetTableAdapters.ProductTableAdapter();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.minPrice = new System.Windows.Forms.TextBox();
+            this.maxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGrid)).BeginInit();
+            this.ascendingToolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDBDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // productDataGrid
+            // 
+            this.productDataGrid.AutoGenerateColumns = false;
+            this.productDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productNumberDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.unitsOnHandDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.productDataGrid.DataSource = this.productBindingSource;
+            this.productDataGrid.Location = new System.Drawing.Point(36, 53);
+            this.productDataGrid.Name = "productDataGrid";
+            this.productDataGrid.RowHeadersWidth = 62;
+            this.productDataGrid.RowTemplate.Height = 28;
+            this.productDataGrid.Size = new System.Drawing.Size(816, 269);
+            this.productDataGrid.TabIndex = 0;
+            // 
+            // ascendingToolStrip
+            // 
+            this.ascendingToolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.ascendingToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ascendingToolStripButton});
+            this.ascendingToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.ascendingToolStrip.Name = "ascendingToolStrip";
+            this.ascendingToolStrip.Size = new System.Drawing.Size(890, 34);
+            this.ascendingToolStrip.TabIndex = 1;
+            this.ascendingToolStrip.Text = "ascendingToolStrip";
+            // 
+            // ascendingToolStripButton
+            // 
+            this.ascendingToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ascendingToolStripButton.Name = "ascendingToolStripButton";
+            this.ascendingToolStripButton.Size = new System.Drawing.Size(99, 29);
+            this.ascendingToolStripButton.Text = "Ascending";
+            this.ascendingToolStripButton.Click += new System.EventHandler(this.ascendingToolStripButton_Click);
+            // 
+            // productNumberDataGridViewTextBoxColumn
+            // 
+            this.productNumberDataGridViewTextBoxColumn.DataPropertyName = "Product_Number";
+            this.productNumberDataGridViewTextBoxColumn.HeaderText = "Product_Number";
+            this.productNumberDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.productNumberDataGridViewTextBoxColumn.Name = "productNumberDataGridViewTextBoxColumn";
+            this.productNumberDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // unitsOnHandDataGridViewTextBoxColumn
+            // 
+            this.unitsOnHandDataGridViewTextBoxColumn.DataPropertyName = "Units_On_Hand";
+            this.unitsOnHandDataGridViewTextBoxColumn.HeaderText = "Units_On_Hand";
+            this.unitsOnHandDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.unitsOnHandDataGridViewTextBoxColumn.Name = "unitsOnHandDataGridViewTextBoxColumn";
+            this.unitsOnHandDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataMember = "Product";
+            this.productBindingSource.DataSource = this.productDBDataSet;
+            // 
+            // productDBDataSet
+            // 
+            this.productDBDataSet.DataSetName = "ProductDBDataSet";
+            this.productDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productTableAdapter
+            // 
+            this.productTableAdapter.ClearBeforeFill = true;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(549, 335);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(128, 38);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // minPrice
+            // 
+            this.minPrice.Location = new System.Drawing.Point(149, 341);
+            this.minPrice.Name = "minPrice";
+            this.minPrice.Size = new System.Drawing.Size(100, 26);
+            this.minPrice.TabIndex = 3;
+            // 
+            // maxPrice
+            // 
+            this.maxPrice.Location = new System.Drawing.Point(402, 341);
+            this.maxPrice.Name = "maxPrice";
+            this.maxPrice.Size = new System.Drawing.Size(100, 26);
+            this.maxPrice.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Minimum Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Maximum Price";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(735, 325);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(96, 58);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Close";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 422);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maxPrice);
+            this.Controls.Add(this.minPrice);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.ascendingToolStrip);
+            this.Controls.Add(this.productDataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productDataGrid)).EndInit();
+            this.ascendingToolStrip.ResumeLayout(false);
+            this.ascendingToolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productDBDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView productDataGrid;
+        private ProductDBDataSet productDBDataSet;
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private ProductDBDataSetTableAdapters.ProductTableAdapter productTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitsOnHandDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStrip ascendingToolStrip;
+        private System.Windows.Forms.ToolStripButton ascendingToolStripButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.TextBox minPrice;
+        private System.Windows.Forms.TextBox maxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ CPT-206-Chapter-14-Problem-6-Viktor-Golovin/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Viktor Golovin
+//Cpt 206
+//Lab1
+//Chapter 14, Problem 6
+
+namespace CPT_206_Chapter_14_Problem_6_Viktor_Golovin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+            //Call productData Context
+            ProductsDataContext db = new ProductsDataContext();
+
+
+        }
+        //Adds a ascending order button to put price into ascending order
+        private void ascendingToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.productTableAdapter.Ascending(this.productDBDataSet.Product);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+        //Calculate button on click event
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            ProductsDataContext db = new ProductsDataContext();
+            //parse minimum price and maximum price as integers
+            int num1 = int.Parse(minPrice.Text);
+            int num2 = int.Parse(maxPrice.Text);
+            //2 if statements to show a message box if 0 is entered
+            if (num2 == 0)
+            {
+                MessageBox.Show("Please Enter a valid number");
+            }
+            if (num1 == 0)
+            {
+                MessageBox.Show("Please Enter a valid number");
+            }
+            
+            var results = from product in db.Products
+                              //Query to select where units on hand are >= num1, or =< num2
+                          where product.Price >= num1
+                          where product.Price <= num2
+                          //Select and return values after query
+                          select product;
+            //display results in the datagrid
+            productDataGrid.DataSource = results;
+        }
+        //Close program button
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    //the end
+}
